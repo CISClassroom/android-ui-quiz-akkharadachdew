fce825408945813ee4b9b6f571a0000de4e3d451
Sareephol <steam.methawee@gmail.com>
2020-03-12 12:56:44 +0700

    603410223-2

---
+++ app/src/main/java/com/cis/lab/uiquiz/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.cis.lab.uiquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main3Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+    }
+}
+
---
+++ app/src/main/java/com/cis/lab/uiquiz/list.kt
@@ -0,0 +1,14 @@
+package com.cis.lab.uiquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class list : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+
+
+    }
+}
